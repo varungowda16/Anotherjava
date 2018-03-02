0063a4d5666a0dc266d347cb7ee20fe2af4861bb
tyss <tyss@localhost>
2018-03-02 10:16:09 +0530

    Another class is modified

--- Anotherproject/src/another_pakage/Another_class.java
+++ Anotherproject/src/another_pakage/Another_class.java
@@ -1,5 +1,14 @@
 package another_pakage;
 
 public class Another_class {
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("sjkdfhksjdhfkjsdhkfjn");
+		
+		
+		System.out.println("5435432");
+	}
 
 }
